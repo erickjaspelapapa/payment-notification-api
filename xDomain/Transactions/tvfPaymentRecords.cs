014efea6f759582694e104cc593d4ce8416d1616
Erickson Papa <ericksonjaspelapapa@gmail.com>
2023-05-06 11:49:28 +0800

    PaymentRecord datas

--- xDomain/Transactions/tvfPaymentRecords.cs
+++ xDomain/Transactions/tvfPaymentRecords.cs
@@ -10,6 +10,7 @@ namespace xDomain.Transactions
     public class tvfPaymentRecords : BaseEntity
     {
         public int? id { get; set; }
+        public bool? forProcessing { get; set; }
         public string? clientId { get; set; }
         public string? client { get; set; }
         public DateTime? latestPayment { get; set; }
@@ -18,13 +19,19 @@ namespace xDomain.Transactions
         public decimal? totalContractPrice { get; set; }
         public decimal? projectedMonPaymentAmt { get; set; }
         public int? monthsToPay { get; set; }
+        public int? monthsPayLeft { get; set; }
         public string? agent { get; set; }
         public decimal? Downpayment { get; set; }
+        public decimal? DownpaymentPerc { get; set; }
         public decimal? Reservation { get; set; }
         public decimal? MonthlyPayment { get; set; }
         public decimal? AgentCommission { get; set; }
+        public decimal? AgentCommsPerc { get; set; }
         public decimal? NotarialFee { get; set; }
         public decimal? Promo { get; set; }
         public decimal? TransferFee { get; set; }
+        public decimal? TotalBalance { get; set; }
+        public decimal? TotalPaid { get; set; }
+        public decimal? TransferFeePaid { get; set; }
     }
 }
